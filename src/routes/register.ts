b976d51607cf2f5a595d9109e81abff2a56cd2a0
miladd3 <milad.d3@gmail.com>
2021-02-08 11:54:11 +0330

    🔏 Hash the password

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,22 +1,34 @@
 import { Router } from 'express';
 import User from '../model/User';
 import { registerValidation } from '../validations';
-
+import bcrypt from 'bcryptjs';
 const router = Router();
 
 router.post('/register', async (req, res) => {
+  // Validate User
   const errors = registerValidation(req.body);
   if (errors) {
     return res.status(400).send(errors);
   }
 
+  // Check if user already exist
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) {
+    return res.status(400).send({ email: '"email" already exist' });
+  }
+
+  // Hash the password
+  const hashedPassword = await bcrypt.hashSync(req.body.password, 10);
+
+  // Create User Model
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
 
   try {
+    // Save User
     const savedUser = await user.save();
     res.send(savedUser.toJSON());
   } catch (err) {
